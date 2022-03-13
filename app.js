f6c4a15e01f992efe79214d3a01871b1179d7f11
vanida gowan <vanidagowan@gmail.com>
2022-03-13 15:38:27 +0700

    The third commit - table page

--- app.js
+++ app.js
@@ -15,8 +15,29 @@ mongoose.connect('mongodb://localhost:27017/cluckDB');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
+var selectedTable = "";
+
 app.get('/', function(req, res) {
     res.render('home');
+})
+
+app.get('/table', function(req, res) {
+    res.render('table');
+})
+
+app.post('/table', function(req, res) {
+    selectedTable = (req.body.table);
+
+    const newItem = new List({
+        table: selectedTable
+    });
+
+    List.insertMany(newItem, function (err) {
+        if (err) 
+          console.log(err);
+    });
+
+    res.redirect('/maindish');
 })
 
 app.listen(8800, function() {
