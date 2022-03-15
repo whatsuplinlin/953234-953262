727ac0fdd0e04bd8b2e930c482ec77830deb102d
whatsuplinlin <kjm010704@gmail.com>
2022-03-15 13:34:06 +0700

    The eighteenth commit

--- app.js
+++ app.js
@@ -612,6 +612,6 @@ app.get("/thankyou", function (req, res) {
     res.render("thankyou")
 })
 
-app.listen(8800, function() {
-    console.log("Server started on port 8800");
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
 });
